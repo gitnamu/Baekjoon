aa9594765457d04a36a7b029877c7408950ccc01
namotree <skagh1750@naver.com>
2020-07-31 18:33:46 +0900

    Baekjoon 11053 C++ O(NlogN) 알고리즘으로 수정

--- 11053.cpp
+++ 11053.cpp
@@ -3,25 +3,26 @@
 
 using namespace std;
 
-int dp[10001], arr[10001];
+int dp[1001] = {0, }, arr[1001] = { 0, };
 
 int main() {
-	int n, mx = 0, arr[1001], dp[1001] = { 0, };
-
+	int n, mx=0, k=1;
 	cin >> n;
-	
+
 	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
-	}
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j < i; j++) {
-			if (arr[i] > arr[j])
-				dp[i] = max(dp[i], dp[j]);
 		}
-		++dp[i];
-		mx = max(dp[i],mx);
+
+	dp[1] = arr[1];
+
+	for (int i = 2; i <= n; i++) {
+				int kcopy = k;
+				if (arr[i] > dp[kcopy])	k++;
+				while (dp[kcopy] >= arr[i]) {
+					--kcopy;
+				}
+				dp[++kcopy] = arr[i];
 	}
 
-	cout << mx;
+	cout << k<<endl;
 }
