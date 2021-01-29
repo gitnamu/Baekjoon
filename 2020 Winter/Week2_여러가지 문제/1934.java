8a03b69d523733655fc0672eb77c48f5dc313c8c
gitnamu <skagh1750@naver.com>
2021-01-29 11:56:02 +0900

    Baekjoon 1934 JAVA

---
+++ 2020 Winter/Week2_여러가지 문제/1934.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			long a = sc.nextInt();
+			long b = sc.nextInt();
+			long a2;
+			long b2;
+			
+			if(b>a) {
+				a2 = b;
+				b2 = a;
+			}else {
+				a2 = a;
+				b2 = b;
+			}
+			while(b2>0) {
+				long tmp = a2;
+				a2 = b2;
+				b2 = tmp % b2;
+			}
+			System.out.println((a*b)/a2);
+		}
+	}
+}
