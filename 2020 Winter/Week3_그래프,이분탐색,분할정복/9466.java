acecc75687ab3504df2ec2816cc4d7a8931256c5
gitnamu <skagh1750@naver.com>
2021-02-03 15:24:08 +0900

    Baekjoon 9466 JAVA

--- 2020 Winter/Week3_그래프,이분탐색,분할정복/9466.java
+++ 2020 Winter/Week3_그래프,이분탐색,분할정복/9466.java
@@ -3,14 +3,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
 import java.util.Stack;
 import java.util.StringTokenizer;
  
 public class Main {
-	static LinkedList<Integer>[] list;
-	static int[] visited;
-	
     public static void main(String[] args) throws IOException{
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,52 +15,44 @@ public class Main {
     	
     	for(int c=0;c<t;c++) {
         	int v = Integer.parseInt(br.readLine());
-        	
-        	visited = new int[v+1];
-        	list = new LinkedList[v+1];
-        	
-        	for(int i=1;i<=v;i++) {
-        		list[i] = new LinkedList<Integer>();
-        	}
+        	boolean[] visited = new boolean[v+1];
+        	int[] list = new int[v+1];
+        	int count = v;
         	
         	String line = br.readLine();
         	StringTokenizer st = new StringTokenizer(line);
         	
         	for(int i=1;i<=v;i++) {
         		int node = Integer.parseInt(st.nextToken());
-        		
-        		list[i].add(node);
+        		list[i] = node;
         	}
-        	
-        	int count = v;
         	
         	for(int i=1;i<=v;i++) {
-        		if(visited[i] == 0) {
-        			count -= check(i);
+        		if(!visited[i]) {
+        			count -= check(list,visited,i);
         		}
         	}
+        	
         	bw.write(count+"\n");
         	bw.flush();
     	}
     }
     
-    public static int check(int start) {
-    	
+    public static int check(int[] list, boolean[] visited, int start) {
     	Stack<Integer> stack = new Stack<Integer>();
-
+    	int count = 0;
     	int nextNode = start;
-    	visited[nextNode] = 1;
+    	
+    	visited[nextNode] = true;
     	stack.push(nextNode);
     	
-    	while(list[nextNode].iterator().hasNext()) {
-    		nextNode = list[nextNode].iterator().next();
-    		if(visited[nextNode] == 0) {
+    	while(true) {
+    		nextNode = list[nextNode];
+    		if(!visited[nextNode]) {
     			stack.push(nextNode);
-    			visited[nextNode] = 1;
+    			visited[nextNode] = true;
     		} else break;
     	}
-    	
-    	int count = 0;
     	
     	while(!stack.isEmpty()) {
     		++count;
